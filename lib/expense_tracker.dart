d9a8a7293b2595801dd5a3abaf49bd9f58f90caf
Mughees Ahmed <mughees.ahmed243@gmail.com>
2025-12-31 08:04:42 +0500

    Add Expense model and implement ExpenseList widget with sample data

---
+++ lib/expense_tracker.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'models/expense.dart';
+import 'expense_list.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.entertainment,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Expense Tracker')),
+        body: Column(
+          children: [
+            const Center(child: Text('Welcome to Expense Tracker!')),
+
+            SizedBox(height: 16),
+            Expanded(child: ExpenseList(expenses: _registeredExpenses)),
+          ],
+        ),
+      ),
+    );
+  }
+}
