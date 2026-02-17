681de84145768c79b06c86fa4a743c45c8085189
Mughees Ahmed <mughees.ahmed243@gmail.com>
2026-02-18 04:01:55 +0500

    Refactor main.dart to enhance MaterialApp theme configuration and structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
 import 'widgets/expense_tracker.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
-  runApp(MaterialApp(home: const Expenses()));
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+
+          // centerTitle: true,
+          // titleSpacing: 100,
+          //   titleTextStyle: const TextStyle(
+          //     fontSize: 20,
+          //     fontWeight: FontWeight.bold,
+          //   ).copyWith(letterSpacing: 20),
+        ),
+      ),
+      home: const Expenses(),
+    ),
+  );
 }
